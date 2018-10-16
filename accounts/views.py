8f7119d31556554101eb172ca2fecd7a6d6b2361
evigne <vigneshwaranethirajan@gmail.com>
2018-10-16 17:42:42 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse('Home Page')
+def home(request):
+    return render(request,'accounts/login.html')
